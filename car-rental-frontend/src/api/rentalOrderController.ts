910fd0f0129b903589074afbeb882df56b77069b
fangxiaolin <1565906107@qq.com>
2025-04-28 12:23:18 +0800

    feat: 订单修改

--- car-rental-frontend/src/api/rentalOrderController.ts
+++ car-rental-frontend/src/api/rentalOrderController.ts
@@ -16,3 +16,41 @@ export async function createRentalOrderUsingPost(
     ...(options || {}),
   })
 }
+
+/** 支付宝异步通知 POST /api/rentalOrder/notify */
+export async function alipayNotifyUsingPost(options?: { [key: string]: any }) {
+  return request<string>('/api/rentalOrder/notify', {
+    method: 'POST',
+    ...(options || {}),
+  })
+}
+
+/** 获取订单列表 POST /api/rentalOrder/page */
+export async function pageRentalOrderUsingPost(
+  body: API.RentalOrderPageRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponsePageRentalOrderVO_>('/api/rentalOrder/page', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
+
+/** 支付订单 POST /api/rentalOrder/pay */
+export async function payOrderUsingPost(
+  body: API.RentalOrderPayRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.BaseResponseString_>('/api/rentalOrder/pay', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  })
+}
